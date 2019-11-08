0efac607f6b400e816293b43c7da8eeecdb0d931
takuya matsunaga <takanotume24@gmail.com>
2019-11-09 00:32:56 +0900

    余計な部分を削除

--- src/main.cpp
+++ src/main.cpp
@@ -23,23 +23,21 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
     if (data[1] != 0x00) return;
     if (data[2] != 0x02) return;
     if (data[3] != 0x15) return;
-    if ((data[0] == 0x4c) && (data[1] == 0x00) && (data[2] == 0x02) &&
-        (data[3] == 0x15)) {
-      sprintf(uuid,
-              "%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%"
-              "02X%02X",
-              data[4], data[5], data[6], data[7], data[8], data[9], data[10],
-              data[11], data[12], data[13], data[14], data[15], data[16],
-              data[17], data[18], data[19]);
-      int major = (int)(((data[20] & 0xff) << 8) + (data[21] & 0xff));
-      int minor = (int)(((data[22] & 0xff) << 8) + (data[23] & 0xff));
-      signed char power = (signed char)(data[24] & 0xff);
-      Serial.printf("addr=%s rssi=%d uuid=%s,major=%d,minor=%d,power=%d\n",
-                    ble_addr.toString().c_str(), rssi, uuid, major, minor,
-                    power);
-    }
+    sprintf(uuid,
+            "%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%"
+            "02X%02X",
+            data[4], data[5], data[6], data[7], data[8], data[9], data[10],
+            data[11], data[12], data[13], data[14], data[15], data[16],
+            data[17], data[18], data[19]);
+    int major = (int)(((data[20] & 0xff) << 8) + (data[21] & 0xff));
+    int minor = (int)(((data[22] & 0xff) << 8) + (data[23] & 0xff));
+    signed char power = (signed char)(data[24] & 0xff);
+    Serial.printf("addr=%s rssi=%d uuid=%s,major=%d,minor=%d,power=%d\n",
+                  ble_addr.toString().c_str(), rssi, uuid, major, minor, power);
   }
-};
+}
+}
+;
 
 void setup() {
   Serial.begin(115200);
